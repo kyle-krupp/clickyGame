b18ef7b3c8010e8f54ba4472aa9d61df33a47493
Kyle Krupp <kyle.krupp@gmail.com>
2018-11-11 10:45:14 -0500

    commit

---
+++ build/precache-manifest.976aed34a6dcea52fbb66fff506d7db4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5e1124212e913e930db",
+    "url": "/clickyGame/static/js/runtime~main.f5e11242.js"
+  },
+  {
+    "revision": "b6ef377d28052105ebad",
+    "url": "/clickyGame/static/js/main.b6ef377d.chunk.js"
+  },
+  {
+    "revision": "b471de583f097f24922e",
+    "url": "/clickyGame/static/js/1.b471de58.chunk.js"
+  },
+  {
+    "revision": "b6ef377d28052105ebad",
+    "url": "/clickyGame/static/css/main.b60e328f.chunk.css"
+  },
+  {
+    "revision": "5394b22b39109c4fccf8c9b8645da032",
+    "url": "/clickyGame/index.html"
+  }
+];
