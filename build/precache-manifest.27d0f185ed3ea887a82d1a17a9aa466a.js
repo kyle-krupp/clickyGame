3c70b1409d996dc61cf30a0715e3c7ce43c76517
Kyle Krupp <kyle.krupp@gmail.com>
2018-11-11 11:34:34 -0500

    read me and deploy

---
+++ build/precache-manifest.27d0f185ed3ea887a82d1a17a9aa466a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "7fbf0e9c9c6ff3616a08",
+    "url": "/ClickyGame/static/js/main.7fbf0e9c.chunk.js"
+  },
+  {
+    "revision": "b471de583f097f24922e",
+    "url": "/ClickyGame/static/js/1.b471de58.chunk.js"
+  },
+  {
+    "revision": "7fbf0e9c9c6ff3616a08",
+    "url": "/ClickyGame/static/css/main.b60e328f.chunk.css"
+  },
+  {
+    "revision": "b1a561674039c6577eb95575227f9323",
+    "url": "/ClickyGame/index.html"
+  }
+];
